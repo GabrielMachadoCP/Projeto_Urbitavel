48dc619375f6fc9b6069d380463e8fd539262ead
bdancs <rm551645@fiap.com.br>
2023-11-12 22:14:43 -0300

    Mais responsividade

--- src/paginas/Privacidade/index.jsx
+++ src/paginas/Privacidade/index.jsx
@@ -11,19 +11,19 @@ export default function Privacidade() {
             <div className="privacidade">
                 <p>
                     É política do Urbitável respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site Urbitável, e outros sites que possuímos e operamos.
-                </p>
+                </p> <br/>
 
                 <p>
                     Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.
-                </p>
+                </p> <br/>
 
                 <p>
                     Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
-                </p>
+                </p> <br/>
 
                 <p>
                     Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.
-                </p>
+                </p> <br/>
 
                 <p>
                     Compromisso do Usuário: O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Urbitável oferece no site e com caráter enunciativo, mas não limitativo:
@@ -31,11 +31,11 @@ export default function Privacidade() {
 
                 <p>
                     <span className="indice">A)</span> Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
-                </p>
+                </p> <br/>
 
                 <p>
                     <span className="indice">B)</span> Não difundir propaganda ou conteúdo de natureza racista, xenofóbica,  betano apostas ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;
-                </p>
+                </p> <br/>
 
                 <p>
                     <span className="indice">C)</span> Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Urbitável, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
